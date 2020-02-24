047baf7ae052b8ac1dda13d47f3e6627e078b332
calebwkang <caleb.kang@unc.edu>
2020-02-24 10:18:10 -0500

    first commit

---
+++ src/scripts.js
@@ -0,0 +1,76 @@
+
+// called when the user clicks the get my pros button
+function getStats()  {
+
+    // get the college selected
+    var collegeSelect = document.getElementById("collegeSelect");
+    var college = collegeSelect.options[collegeSelect.selectedIndex];
+
+    // get the league seleted
+    var leagueSelect = document.getElementById("leagueSelect");
+    var league = leagueSelect.options[leagueSelect.selectedIndex];
+
+    // ensures user has selected both values
+    if (college.value == "default" || league.value == "default") {
+
+        this.alert("Select a college and a league");
+
+    } else {
+
+        // makes query
+        var data = makeQuery(college.text, league.text);
+
+    }
+}
+
+// In: two strings representing the college and the league
+// Out: an array out player objects from college and in league
+function makeQuery(college, league) {
+    var data = null;
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.responseType = "json";
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+
+            data = this.response;
+
+            var players = data.api.players
+
+            var filtered = filteredByCollege(players, college)
+
+            // display data
+            display(filtered);
+
+        }
+    });
+
+    xhr.open("GET", "https://api-nba-v1.p.rapidapi.com/players/league/standard");
+    xhr.setRequestHeader("x-rapidapi-host", "api-nba-v1.p.rapidapi.com");
+    xhr.setRequestHeader("x-rapidapi-key", "ab71a41c90mshf97209670d5d649p1c1145jsn9b9737f9b673");
+    xhr.send(data);
+
+
+}
+
+function display(data) {
+    console.log(data);
+}
+
+function filteredByCollege(players, college) {
+    
+    var result = [];
+
+    for (i=0; i<players.length; i++) {
+        if (players[i].collegeName.localeCompare(college) == 0) {
+            result.push(players[i]);
+        }
+    }
+
+    return result;
+    
+}
+
+
