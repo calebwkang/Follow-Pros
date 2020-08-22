91b80dbbb724d8c8eedd13cc4768c6afc175a37c
calebwkang <caleb.kang@unc.edu>
2020-08-22 18:30:27 -0400

    delete extra promise code

--- src/model.js
+++ src/model.js
@@ -1,13 +1,7 @@
 class Player {
     constructor(firstName, lastName, playerId) {
         this.firstName = firstName; this.lastName = lastName;
-        this.playerId = playerId; this.games = null;
-
-        this.promise = new Promise((resolve, reject) => {
-            if (this.games != null) {
-                resolve(this)
-            }
-        })
+        this.playerId = playerId; this.games = null; this.promise = null;
     }
 
     getFullName() {
